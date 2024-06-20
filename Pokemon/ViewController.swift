7b495b70b66037cee3db61274984b0c3790c9949
IanFan <ianfan0704@gmail.com>
2024-06-20 15:00:15 +0800

    App: build new project, support iOS 16, portrait

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Ian Fan on 2024/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
